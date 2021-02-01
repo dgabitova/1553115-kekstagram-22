297157dec333a8119c6f4b8b78487c581ee0e9bf
dgabitova <70890210+dgabitova@users.noreply.github.com>
2021-02-01 10:17:17 +0500

    Правки к функциям

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-// Get random number, https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-const getRandomInt = function (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// Get random number
+const getRandomNum =  function(minNumber, maxNumber) {
+  const randomNum = Math.floor(Math.random() * maxNumber);
+  return randomNum > minNumber ? randomNum : minNumber;
+};
+getRandomNum(0, 500);
 
-// Get max string value, https://www.cyberforum.ru/javascript/thread1165903.html
-const getLongestString = function (string max) {
-  if (string.length > max)  {
-    alert ('Длина строки не должна превышать ${max} символов');
+// Get max string value
+const checkLengthString = (string, max = 140) => {
+  string.length;
+  if (string.length <= max) {
+    return true;
   }
+  return false;
 }
-
-let comment = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-getRandomInt(0, 1);
-getLongestStringString(comment, 140);
-
-
+checkLengthString('Lorem ipsum dolor sit amet, consectetur adipiscing elit...');
