47b84b87089481c5023b24a2a9bc54aaf5e8e9b6
dgabitova <70890210+dgabitova@users.noreply.github.com>
2021-02-10 10:51:21 +0500

    Разделила код на модули

---
+++ js/utils.js
@@ -0,0 +1,15 @@
+export {getRandomNum, checkLengthString};
+
+// Get random number
+const getRandomNum = function(minNumber, maxNumber) {
+  const randomNum = Math.floor(Math.random() * maxNumber);
+  return randomNum > minNumber ? randomNum : minNumber;
+};
+getRandomNum(0, 500);
+
+
+// Get max string value
+const checkLengthString = (string, max) => {
+  return string.length <= max;
+}
+checkLengthString ('comment', 140);
