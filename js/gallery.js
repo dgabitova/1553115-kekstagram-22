15f7e44cf9a04e07a137b14fb4e8a1e98ab44445
dgabitova <70890210+dgabitova@users.noreply.github.com>
2021-02-15 14:15:58 +0500

    Разделение на функции

--- js/gallery.js
+++ js/gallery.js
@@ -1,21 +1,45 @@
 import {getRandomUsersArray} from './data.js';
 
+//const photosList = document.querySelector('.pictures');
+//const pictureTemplate = document.querySelector('#picture')
+//  .content
+//  .querySelector('.picture');
+
+//const similarPhotos = getRandomUsersArray();
+
+//const pictureFragment = document.createDocumentFragment();
+
+//similarPhotos.forEach(({url, likes, comments}) => {
+//  const pictureElement = pictureTemplate.cloneNode(true);
+//  pictureElement.querySelector('.picture__img').src = url;
+//  pictureElement.querySelector('.picture__likes').textContent = comments.length;
+//  pictureElement.querySelector('.picture__comments').textContent = likes;
+//  pictureFragment.appendChild(pictureElement);
+//});
+
+//photosList.appendChild(pictureFragment);
+
 const photosList = document.querySelector('.pictures');
+const similarPhotos = getRandomUsersArray();
 const pictureTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-const similarPhotos = getRandomUsersArray();
-
-const pictureFragment = document.createDocumentFragment();
-
-similarPhotos.forEach(({url, likes, comments}) => {
+const getPhotoTemplate = (object) => {
   const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = url;
-  pictureElement.querySelector('.picture__likes').textContent = comments.length;
-  pictureElement.querySelector('.picture__comments').textContent = likes;
-  pictureFragment.appendChild(pictureElement);
-});
+  pictureElement.querySelector('.picture__img').src = object.url;
+  pictureElement.querySelector('.picture__likes').textContent = object.comments.length;
+  pictureElement.querySelector('.picture__comments').textContent = object.likes;
 
-photosList.appendChild(pictureFragment);
+  return pictureElement;
+}
 
+const drawPhotos = (object) => {
+  const pictureFragment = document.createDocumentFragment();
+  object.forEach((item) => {
+    pictureFragment.appendChild(getPhotoTemplate(item));
+  });
+  return pictureFragment;
+}
+
+photosList.appendChild(drawPhotos(similarPhotos));
