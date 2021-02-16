244dc21af45aa292d729500291ede1edf0c47938
dgabitova <70890210+dgabitova@users.noreply.github.com>
2021-02-16 10:17:11 +0500

    Внесены правки

--- js/gallery.js
+++ js/gallery.js
@@ -1,24 +1,5 @@
 import {getRandomUsersArray} from './data.js';
 
-//const photosList = document.querySelector('.pictures');
-//const pictureTemplate = document.querySelector('#picture')
-//  .content
-//  .querySelector('.picture');
-
-//const similarPhotos = getRandomUsersArray();
-
-//const pictureFragment = document.createDocumentFragment();
-
-//similarPhotos.forEach(({url, likes, comments}) => {
-//  const pictureElement = pictureTemplate.cloneNode(true);
-//  pictureElement.querySelector('.picture__img').src = url;
-//  pictureElement.querySelector('.picture__likes').textContent = comments.length;
-//  pictureElement.querySelector('.picture__comments').textContent = likes;
-//  pictureFragment.appendChild(pictureElement);
-//});
-
-//photosList.appendChild(pictureFragment);
-
 const photosList = document.querySelector('.pictures');
 const similarPhotos = getRandomUsersArray();
 const pictureTemplate = document.querySelector('#picture')
@@ -34,9 +15,9 @@ const getPhotoTemplate = (object) => {
   return pictureElement;
 }
 
-const drawPhotos = (object) => {
+const drawPhotos = (similarPhotos) => {
   const pictureFragment = document.createDocumentFragment();
-  object.forEach((item) => {
+  similarPhotos.forEach((item) => {
     pictureFragment.appendChild(getPhotoTemplate(item));
   });
   return pictureFragment;
